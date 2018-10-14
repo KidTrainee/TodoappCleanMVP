47ab5854e3d61669ebd2af1cc7fe42fc7a3099e2
KidTrainee <tailieu.binh.01@gmail.com>
2018-10-15 00:07:43 +0700

    Initial commit

---
+++ app/src/main/java/vn/bfc/todoappcleanmvp/tasks/TasksActivity.java
@@ -0,0 +1,15 @@
+package vn.bfc.todoappcleanmvp.tasks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.bfc.todoappcleanmvp.R;
+
+public class TasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+    }
+}
