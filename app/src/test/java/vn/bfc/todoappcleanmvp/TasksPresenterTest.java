0306aaf530f1bc234282b1bf39417868c7345642
KidTrainee <tailieu.binh.01@gmail.com>
2018-10-24 23:13:23 +0700

    1

--- app/src/test/java/vn/bfc/todoappcleanmvp/TasksPresenterTest.java
+++ app/src/test/java/vn/bfc/todoappcleanmvp/TasksPresenterTest.java
@@ -1,16 +1,30 @@
 package vn.bfc.todoappcleanmvp;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
 import vn.bfc.todoappcleanmvp.data.source.TasksDataSource;
 import vn.bfc.todoappcleanmvp.data.source.TasksRepository;
+import vn.bfc.todoappcleanmvp.domain.filter.FilterFactory;
 import vn.bfc.todoappcleanmvp.domain.model.Task;
+import vn.bfc.todoappcleanmvp.domain.usecase.UCActivateTask;
+import vn.bfc.todoappcleanmvp.domain.usecase.UCClearCompleteTasks;
+import vn.bfc.todoappcleanmvp.domain.usecase.UCCompleteTask;
+import vn.bfc.todoappcleanmvp.domain.usecase.UCGetTasks;
 import vn.bfc.todoappcleanmvp.tasks.TasksContract;
+import vn.bfc.todoappcleanmvp.tasks.TasksFilterType;
 import vn.bfc.todoappcleanmvp.tasks.TasksPresenter;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.verify;
 
 /**
  * Unit tests for the implementation of {@link TasksPresenter}
@@ -30,5 +44,60 @@ public class TasksPresenterTest {
      */
     @Captor
     private ArgumentCaptor<TasksDataSource.LoadTasksCallback> mLoadTasksCallbackCaptor;
+
+    private TasksPresenter mTasksPresenter;
+
+    @Before
+    public void setupTasksPresenter() {
+        // Mockito has a very convenient way to inject mocks by using the @Mock annotation.
+        // To inject the mocks in the test the initMocks method needs to be called.
+        MockitoAnnotations.initMocks(this);
+
+        // Get a reference to the class under test
+        mTasksPresenter = givenTasksPresenter();
+
+    }
+
+    private TasksPresenter givenTasksPresenter() {
+        UseCaseHandler useCaseHandler = new UseCaseHandler(new TestUseCaseScheduler());
+        UCGetTasks getTasks = new UCGetTasks(mTasksRepository, new FilterFactory());
+        UCCompleteTask completeTask = new UCCompleteTask(mTasksRepository);
+        UCActivateTask activateTask = new UCActivateTask(mTasksRepository);
+        UCClearCompleteTasks clearCompleteTasks = new UCClearCompleteTasks(mTasksRepository);
+
+        return new TasksPresenter(useCaseHandler, mTasksView, getTasks, completeTask, activateTask,
+                clearCompleteTasks);
+    }
+
+    @Test
+    public void createPresenter_setsThePresenterToView() {
+        // Get a reference to the class under test
+        mTasksPresenter = givenTasksPresenter();
+
+        // Then the presenter is set to the view
+        verify(mTasksView).setPresenter(mTasksPresenter);
+    }
+    @Test
+    public void loadAllTasksFromRepositoryAndLoadIntoView() {
+        // Given an initialized TasksPresenter with initialized tasks
+        // When loading of Tasks is requested
+        mTasksPresenter.setFiltering(TasksFilterType.ALL_TASKS);
+        mTasksPresenter.loadTasks(true);
+
+        // Callback is captured and invoked with stubbed tasks
+        verify(mTasksRepository).getTasks(mLoadTasksCallbackCaptor.capture());
+        mLoadTasksCallbackCaptor.getValue().onTasksLoaded(TASKS);
+
+        // Then progress indicator is shown
+        InOrder inOrder = inOrder(mTasksView);
+        inOrder.verify(mTasksView).setLoadingIndicator(true);
+        // Then progress indicator is hidden and all tasks are shown in UI
+        inOrder.verify(mTasksView).setLoadingIndicator(false);
+        ArgumentCaptor<List> showTasksArgumentCaptor = ArgumentCaptor.forClass(List.class);
+        verify(mTasksView).showTasks(showTasksArgumentCaptor.capture());
+        assertTrue(showTasksArgumentCaptor.getValue().size() == 3);
+    }
+
+
 
 }
